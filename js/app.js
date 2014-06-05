f4885197a2ef4689d1e8d451369138aab2008af8
Muhammad Ashraf Ishak <m.ashraf.ishak@gmail.com>
2014-06-05 20:26:38 +0800

    First commit

---
+++ js/app.js
@@ -0,0 +1,43 @@
+var app = angular.module('nvp-tool',[]);
+
+
+app.controller("mainController", function($scope, $http){
+	$scope.pairArray = new Array();
+	$scope.debug = "";
+	$scope.input = "";
+	$scope.inputChanged = function(){
+		//$scope.debug = debugArray(splitNVP($scope.input));
+		$scope.pairArray = debugArray(splitNVP($scope.input));
+	};
+    $scope.init = function() {
+    };
+	
+	function checkFormat(inputStr){
+		// TODO 
+	}
+	function splitNVP(inputStr){
+		var amp =  inputStr.split('&');
+		var result = new Array();
+		for (var i = 0; i < amp.length; i++){
+			var pair = amp[i].split('=');
+			result[i] = new Array();
+			result[i]['key'] = pair[0];
+			result[i]['value'] = pair[1];
+		}
+		return result;
+	}
+	
+	// FOR TESTING PURPOSE ONLY
+	function debugArray(array){
+		var result = "";
+		for (var i = 0; i < array.length; i++){
+			result = result + array[i]['key'];
+			result = result + " => ";
+			result = result + array[i]['value'];
+			result = result + ";";
+		}
+		return result;
+	}
+	
+    
+});
