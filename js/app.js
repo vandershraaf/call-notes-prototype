940c65ba5053e03308d422739c7096dfd2919f8f
Muhammad Ashraf Ishak <m.ashraf.ishak@gmail.com>
2014-07-20 10:26:22 +0800

    Almost done i guess

--- js/app.js
+++ js/app.js
@@ -1,43 +1,82 @@
-var app = angular.module('nvp-tool',[]);
-
+var app = angular.module('calltransfer',[]);
 
 app.controller("mainController", function($scope, $http){
-	$scope.pairArray = new Array();
-	$scope.debug = "";
-	$scope.input = "";
-	$scope.inputChanged = function(){
-		//$scope.debug = debugArray(splitNVP($scope.input));
-		$scope.pairArray = debugArray(splitNVP($scope.input));
-	};
-    $scope.init = function() {
-    };
-	
-	function checkFormat(inputStr){
-		// TODO 
+
+	var host_url = "http://localhost:8080/services/";
+	/* FOR TESTING ONLY */
+	var username = "mishak";
+	var api_token = "hoc0297702ia0kfkpyn8";
+	//$scope.get_result = {};
+	$scope.transfer_list = [];
+	var request = {};
+    var get_result = {};
+    $scope.today_text = "";
+
+	$scope.init = function (){
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var mmStr = "";
+        var yyyy = today.getFullYear();
+        if(dd<10) {
+            dd='0'+dd
+        } 
+
+        if(mm<10) {
+            mmStr='0'+mm
+        } 
+        var months = ["January", "February", "March", "April", "May", "June", "July","August", "September","October","November","December"];
+        $scope.date_input = yyyy+'-'+mm+'-'+dd;
+        $scope.today_text = dd+' '+months[mm - 1]+' '+yyyy;
+		getTodayTransfer();
 	}
-	function splitNVP(inputStr){
-		var amp =  inputStr.split('&');
-		var result = new Array();
-		for (var i = 0; i < amp.length; i++){
-			var pair = amp[i].split('=');
-			result[i] = new Array();
-			result[i]['key'] = pair[0];
-			result[i]['value'] = pair[1];
-		}
-		return result;
+
+	$scope.add_transfer = function (){
+        //console.log("This is a test");
+		request.agent = $scope.agent_input;
+		request.account = $scope.account_input;
+		request.date = $scope.date_input;
+		request.user = username;
+		request.apiToken = api_token;
+        //console.log(request);
+		getResultPOST(host_url+"transfers/transfer/add", request);
+	}    
+
+	function getTodayTransfer(){
+		getResultGET(host_url+"transfers/today?user="+username+"&apiToken="+api_token);
+        //$scope.transfer_list = $scope.transfer_list.transferList;
+		//console.log($scope.transfer_list);
 	}
-	
-	// FOR TESTING PURPOSE ONLY
-	function debugArray(array){
-		var result = "";
-		for (var i = 0; i < array.length; i++){
-			result = result + array[i]['key'];
-			result = result + " => ";
-			result = result + array[i]['value'];
-			result = result + ";";
-		}
-		return result;
-	}
-	
-    
+
+	function getResultGET(url){
+        $http.get(url).success(function(data) {
+            //console.log(data);
+            $scope.transfer_list = data.transferList;
+        }).error(function(error) {
+ 
+        });
+    }
+
+    function getResultPOST(url, data){
+        $http.post(url, data).success(function(data) {
+            //console.log(data);
+            var added = {};
+            added.account = {};
+            added.agent = {};
+            added.agent.username = $scope.agent_input;
+            added.account.acctNum = $scope.account_input;
+            $scope.transfer_list.push(added);
+            console.log($scope.transfer_list);
+            $('#myModal').modal();
+        }).error(function(error) {
+ 			
+        });
+    }
+
+
 });
+
+
+
+
+
